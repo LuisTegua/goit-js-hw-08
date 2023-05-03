ec43e9440dc4b9cd3f93f113fde95494f1510125
LuisETeguaS <tegualuis2@gmail.com>
2023-05-03 09:42:03 -0500

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+let values = {};
+
+
+values = JSON.parse(localStorage.getItem("feedback-form-state"));
+if (values != null) {
+  form.querySelectorAll('[name]').forEach((event) => {
+    event.value = values[event.name];
+  });
+} else
+  values = {};
+
+
+const asignacionEvent = (e) => {
+  values[e.target.name] = e.target.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(values));
+}
+
+const submit = (e) => {
+  console.log(localStorage.getItem("feedback-form-state"));
+  localStorage.clear();
+  form.reset();
+  e.preventDefault();
+}
+
+form.addEventListener('input', throttle(asignacionEvent, 500));
+form.addEventListener('submit', submit);
